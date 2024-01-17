282eda1220e853d6a7fe099c4e793662c869d530
cesarbeloqui <beloqui.cesar@gmail.com>
2024-01-17 05:19:31 -0300

    Mostrando el Nombre del Proyecto

--- cliente/src/components/tareas/ListadoTareas.jsx
+++ cliente/src/components/tareas/ListadoTareas.jsx
@@ -1,6 +1,17 @@
 import Tarea from "./Tarea";
+import proyectoContext from "../../context/proyectos/proyectoContext";
+import { useContext } from "react";
 
 const ListadoTareas = () => {
+  //Extraer proyectos de state inicial
+  const proyectosContext = useContext(proyectoContext);
+  const { proyecto } = proyectosContext;
+
+  //Si no hay proyecto seleccionado, retornar un mensaje
+  if (!proyecto) return <h1>Selecciona un proyecto</h1>;
+  //Array destructuring para extraer el proyecto actual:
+  const [proyectoActual] = proyecto;
+
   const tareasProyecto = [
     { id: 1, nombre: "Tarea 1", estado: true },
     { id: 2, nombre: "Tarea 2", estado: false },
@@ -9,7 +20,7 @@ const ListadoTareas = () => {
   ];
   return (
     <>
-      <h2>Proyecto: Tienda Virtual</h2>
+      <h2>Proyecto: {proyectoActual.nombre}</h2>
       <ul className="listado-tareas">
         {tareasProyecto.length === 0 ? (
           <li className="tarea">
